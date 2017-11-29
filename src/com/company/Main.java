0a7e4e2c97d1bbeef7ade9cd36c5d7d79851ba85
jebyson <joshuaebyson@yahoo.com>
2017-11-29 18:05:11 -0500

    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        String t1 = cat(68, 108);
+        System.out.println(t1);
 
+        String t2 = cat(35, 59);
+        System.out.println(t2);
+    }
+
+    private static String cat(int start, int end) {
+        StringBuffer sb = new StringBuffer("");
+
+        for (int j = start; j <= end; j++) {
+            sb.append((char)(j));
+        }
+        sb.append('\n');
+
+        String s = sb.toString();
+        return s;
     }
 }
